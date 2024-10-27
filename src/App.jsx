3b111ad4ce0e8f4900494da89bba56087217c2f3
Masum Ali <rimasum.dev@gmail.com>
2024-10-27 20:06:23 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
